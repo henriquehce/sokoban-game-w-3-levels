39d9a9abfc2d0fa32890e28bd7efcd8f97454d3b
Henrique Cipriani <113396592+henriquehce@users.noreply.github.com>
2023-03-20 20:03:03 -0300

    Update jogo.cpp

--- jogo.cpp
+++ jogo.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <locale.h>
+#include <cstdlib> // para rand()
 
 using namespace std;
 
 void exibir_menu();
 void exibir_sobre();
+void novo_jogo();
 
 int main() {
     setlocale(LC_ALL, "Portuguese");
@@ -20,28 +22,25 @@ int main() {
             cin.clear(); // Limpa o buffer
             cin.ignore(); // Ignora o caractere digitado
             system("cls"); // Limpa a tela
-            cout << "\nOpção inválida\n";
+            cout << "\nOpÃ§Ã£o invÃ¡lida\n";
             continue; // Reinicia o loop
         }
 
         switch (opcao) {
             case 1:
-                system("cls"); // Limpa a tela
-                cout << "\nOpção escolhida: Novo Jogo\n";
+                novo_jogo();
                 break;
             case 2:
-                system("cls"); // Limpa a tela
-                cout << "\nOpção escolhida: Continuar\n";
+                cout << "\nOpÃ§Ã£o escolhida: Continuar\n";
                 break;
             case 3:
-                system("cls"); // Limpa a tela
                 exibir_sobre();
                 break;
             case 4:
+                cout << "\nOpÃ§Ã£o escolhida: Fim\n";
                 break;
             default:
-                system("cls"); // Limpa a tela
-                cout << "\nOpção inválida\n";
+                cout << "\nOpÃ§Ã£o invÃ¡lida\n";
                 break;
         }
 
@@ -49,7 +48,7 @@ int main() {
             cout << "\nPressione qualquer tecla para continuar...";
             cin.ignore();
             cin.get();
-            system("cls"); // Limpa a tela
+            system("cls");
         }
     }
 
@@ -63,19 +62,74 @@ void exibir_menu() {
     cout << "3 - Sobre\n";
     cout << "4 - Fim\n";
     cout << "============\n";
-    cout << "Escolha uma opção: ";
+    cout << "Escolha uma opÃ§Ã£o: ";
 }
 
 void exibir_sobre() {
     cout << "\n=== SOBRE ===\n";
-    cout << "Alunos: Fernando Galvão Willemann, Henrique Borovicz Reis, Henrique Cipriani Ertal\n";
+    cout << "Alunos: Fernando GalvÃ£o Willemann, Henrique Borovicz Reis, Henrique Cipriani Ertal\n";
     cout << "Professor: Thiago Felski Pereira\n";
     cout << "Regras do jogo Sokoban:\n";
-    cout << "- O jogador movimenta o personagem para empurrar as caixas até os locais indicados no mapa.\n";
+    cout << "- O jogador movimenta o personagem para empurrar as caixas atÃ© os locais indicados no mapa.\n";
     cout << "- O jogo termina quando todas as caixas estiverem nos locais indicados.\n";
-    cout << "- O personagem não pode empurrar mais de uma caixa ao mesmo tempo.\n";
-    cout << "- O personagem não pode puxar as caixas.\n";
-    cout << "- O personagem não pode passar através das caixas.\n";
-    cout << "- O personagem não pode passar através das paredes.\n";
-    cout << "Março 2023.\n";
+    cout << "- O personagem nÃ£o pode empurrar mais de uma caixa ao mesmo tempo.\n";
+    cout << "- O personagem nÃ£o pode puxar as caixas.\n";
+    cout << "- O personagem nÃ£o pode passar atravÃ©s das caixas.\n";
+    cout << "- O personagem nÃ£o pode passar atravÃ©s das paredes.\n";
+    cout << "- Boa sorte e divirta-se!\n";
+    cout << "InformaÃ§Ãµes do mÃªs de marÃ§o de 2023.\n";
+}
+
+void novo_jogo() {
+    cout << "\nOpÃ§Ã£o escolhida: Novo Jogo\n";
+
+    int opcao_mapa = 0;
+
+    while (opcao_mapa != 5) {
+        cout << "\nEscolha o mapa:\n";
+        cout << "1 - Mapa 1\n";
+        cout << "2 - Mapa 2\n";
+        cout << "3 - Mapa 3\n";
+        cout << "4 - AleatÃ³rio\n";
+        cout << "5 - Voltar\n";
+        cout << "============\n";
+        cout << "Escolha uma opÃ§Ã£o: ";
+
+        cin >> opcao_mapa;
+
+        switch (opcao_mapa) {
+            case 1:
+                cout << "\nMapa escolhido: Mapa 1\n";
+                break;
+            case 2:
+                cout << "\nMapa escolhido: Mapa 2\n";
+                break;
+            case 3:
+                cout << "\nMapa escolhido: Mapa 3\n";
+                break;
+            case 4:
+                cout << "\nMapa escolhido: AleatÃ³rio\n";
+                break;
+            case 5:
+                cout << "\nOpÃ§Ã£o escolhida: Voltar\n";
+                break;
+            default:
+                cout << "\nOpÃ§Ã£o invÃ¡lida\n";
+                break;
+        }
+
+        if (opcao_mapa != 5) {
+            cout << "\nPressione qualquer tecla para continuar...";
+            cin.ignore();
+            cin.get();
+            system("cls");
+        }
+        if (cin.fail()) { 
+            cin.clear(); 
+            cin.ignore(); 
+            system("cls"); 
+            cout << "\nOpÃ§Ã£o invÃ¡lida\n";
+            continue; 
+        }
+    }
 }
